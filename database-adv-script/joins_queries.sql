41f6a8017b0ecc3951c4297c4470f0770972c40f
Avstel <it.nathan.demissie@gmail.com>
2025-09-14 15:14:14 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,9 +1,4 @@
-SELECT users.*, bookings.*
-FROM users
-LEFT JOIN bookings ON users.id = bookings.user_id
-
-UNION
-
-SELECT users.*, bookings.*
-FROM users
-RIGHT JOIN bookings ON users.id = bookings.user_id;
+-- Retrieve all bookings and the respective users who made those bookings
+SELECT bookings.*, users.*
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
