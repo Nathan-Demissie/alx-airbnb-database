fc4f710ad6cf81ae101b2b8eff8ef353a600b37c
Avstel <it.nathan.demissie@gmail.com>
2025-09-14 15:21:11 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,14 +3,13 @@ SELECT bookings.*, users.*
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
--- Users with or without bookings
+-- Simulate FULL OUTER JOIN in SQLite using UNION of two LEFT JOINs
 SELECT users.*, bookings.*
 FROM users
 LEFT JOIN bookings ON users.id = bookings.user_id
 
 UNION
 
--- Bookings with or without users
 SELECT users.*, bookings.*
 FROM bookings
 LEFT JOIN users ON bookings.user_id = users.id;
